53834b0466402267a1a4b716f8977f9b019fd84d
Prakhar Kulshrestha <prakharkul30497@gmail.com>
2018-06-05 10:50:36 +0530

    First Commit

---
+++ app/src/main/java/in/prakhar/textrecognition/MainActivity.java
@@ -0,0 +1,187 @@
+package in.prakhar.textrecognition;
+
+import android.*;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextDetector;
+
+import org.w3c.dom.Text;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_STORAGE_PERMISSION = 2;
+
+    private static final String FILE_PROVIDER_AUTHORITY = "in.prakhar.fileprovider1";
+
+    private String mTempPhotoPath;
+
+    private Bitmap mResultsBitmap;
+
+    private ImageView mImageView;
+    private TextView mText;
+    private Button mButton;
+    private FloatingActionButton mSaveFab;
+    private FloatingActionButton mShareFab;
+    private FloatingActionButton mClearFab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mImageView = (ImageView) findViewById(R.id.image_view);
+        mText = (TextView) findViewById(R.id.text);
+        mButton = (Button) findViewById(R.id.button);
+        mSaveFab = (FloatingActionButton) findViewById(R.id.save_button);
+        mShareFab = (FloatingActionButton) findViewById(R.id.share_button);
+        mClearFab = (FloatingActionButton) findViewById(R.id.clear_button);
+    }
+
+    public void openCamera(View view) {
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // If you do not have permission, request it
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_STORAGE_PERMISSION);
+        } else {
+            // Launch the camera if the permission exists
+            launchCamera();
+        }
+    }
+
+    private void launchCamera() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the temporary File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = BitmapUtils.createTempImageFile(this);
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+
+                // Get the path of the temporary file
+                mTempPhotoPath = photoFile.getAbsolutePath();
+
+                // Get the content URI for the image file
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        FILE_PROVIDER_AUTHORITY,
+                        photoFile);
+
+                // Add the URI so the camera can store the image
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+
+                // Launch the camera activity
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // If the image capture activity was called and was successful
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            // Process the image and set it to the TextView
+            processAndSetImage();
+        } else {
+
+            // Otherwise, delete the temporary image file
+            BitmapUtils.deleteImageFile(this, mTempPhotoPath);
+        }
+    }
+
+    /**
+     * Method for processing the captured image and setting it to the TextView.
+     */
+    private void processAndSetImage() {
+
+        // Toggle Visibility of the views
+        mButton.setVisibility(View.GONE);
+        mSaveFab.setVisibility(View.VISIBLE);
+        mShareFab.setVisibility(View.VISIBLE);
+        mClearFab.setVisibility(View.VISIBLE);
+
+        // Resample the saved image to fit the ImageView
+        mResultsBitmap = BitmapUtils.resamplePic(this, mTempPhotoPath);
+
+        // Set the new bitmap to the ImageView
+        mImageView.setImageBitmap(mResultsBitmap);
+
+        textRecognition(mResultsBitmap);
+    }
+
+    private void textRecognition(Bitmap bitmap) {
+
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+
+        FirebaseVisionTextDetector detector = FirebaseVision.getInstance()
+                .getVisionTextDetector();
+        Task<FirebaseVisionText> result =
+                detector.detectInImage(image)
+                        .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+                            @Override
+                            public void onSuccess(FirebaseVisionText texts) {
+                                processTextRecognitionResult(texts);
+                            }
+                        })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                });
+    }
+
+    private void processTextRecognitionResult(FirebaseVisionText texts) {
+        for (FirebaseVisionText.Block block: texts.getBlocks()) {
+            String blockText = block.getText();
+            mText.setText(blockText);
+        }
+    }
+
+    public void clearImage(View view) {
+        mImageView.setImageResource(0);
+        mButton.setVisibility(View.VISIBLE);
+        mShareFab.setVisibility(View.GONE);
+        mSaveFab.setVisibility(View.GONE);
+        mClearFab.setVisibility(View.GONE);
+
+        // Delete the temporary image file
+        BitmapUtils.deleteImageFile(this, mTempPhotoPath);
+    }
+}
